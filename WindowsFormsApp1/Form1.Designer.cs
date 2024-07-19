3d7389b00b2c9d86f5c6ffd9df3c82d2ad6f8684
HUAWEİ <HUAWE@HAFIZE>
2024-07-19 17:32:35 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_ara = new System.Windows.Forms.Button();
+            this.txt_ara = new System.Windows.Forms.TextBox();
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1319, 3);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Git";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(96, 36);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Geri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(251, 39);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 46);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "İleri";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(406, 40);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 44);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Yenile";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1290, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(555, 37);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 45);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "AnaSayfa";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApp1.Properties.Resources.Custom_Icon_Design_Mono_General_3_Home_48;
+            this.pictureBox4.InitialImage = null;
+            this.pictureBox4.Location = new System.Drawing.Point(485, 36);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(64, 75);
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.Iconsmind_Outline_Next_48;
+            this.pictureBox3.InitialImage = null;
+            this.pictureBox3.Location = new System.Drawing.Point(185, 36);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 64);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.Iconsmind_Outline_Back_Music_48;
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(29, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 64);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Iconsmind_Outline_Refresh_48;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(340, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 63);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn_ara);
+            this.panel1.Controls.Add(this.txt_ara);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1443, 113);
+            this.panel1.TabIndex = 12;
+            // 
+            // btn_ara
+            // 
+            this.btn_ara.Location = new System.Drawing.Point(971, 36);
+            this.btn_ara.Name = "btn_ara";
+            this.btn_ara.Size = new System.Drawing.Size(81, 29);
+            this.btn_ara.TabIndex = 13;
+            this.btn_ara.Text = "Ara";
+            this.btn_ara.UseVisualStyleBackColor = true;
+            this.btn_ara.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn_ara_MouseClick);
+            // 
+            // txt_ara
+            // 
+            this.txt_ara.Location = new System.Drawing.Point(784, 39);
+            this.txt_ara.Name = "txt_ara";
+            this.txt_ara.Size = new System.Drawing.Size(181, 22);
+            this.txt_ara.TabIndex = 12;
+            // 
+            // webView2
+            // 
+            this.webView2.AllowExternalDrop = true;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView2.Location = new System.Drawing.Point(0, 0);
+            this.webView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(1264, 447);
+            this.webView2.Source = new System.Uri("http://www.google.com.tr/", System.UriKind.Absolute);
+            this.webView2.TabIndex = 6;
+            this.webView2.ZoomFactor = 1D;
+            this.webView2.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.webView2_NavigationCompleted);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.treeView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 113);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(179, 447);
+            this.panel2.TabIndex = 13;
+         
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(179, 447);
+            this.treeView1.TabIndex = 8;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect_1);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.webView2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(179, 113);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1264, 447);
+            this.panel3.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1443, 560);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Web Tarayıcısı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_ara;
+        private System.Windows.Forms.Button btn_ara;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Web.WebView2.Core;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Security.Policy;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true; // Ana forma klavye olaylarını yönlendirme
+            this.KeyDown += button1_KeyDown;
+        }
+
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            txt_ara.Visible = false;
+            btn_ara.Visible = false;
+            //treeview yapısı
+            await InitializeWebView2Async();
+            treeView1.Nodes.Add("SOSYAL MEDYA");
+            treeView1.Nodes[0].Nodes.Add("Instagram").Tag=1;
+            treeView1.Nodes[0].Nodes.Add("X").Tag=2;
+            
+            treeView1.Nodes[0].Nodes.Add("Facebook").Tag=3;
+            treeView1.Nodes.Add("TARAYICI");
+            treeView1.Nodes[1].Nodes.Add("Google").Tag=4;
+            treeView1.Nodes[1].Nodes.Add("Yandex").Tag=5;
+            treeView1.Nodes[1].Nodes.Add("Yahoo").Tag=6;
+            treeView1.Nodes[1].Nodes.Add("Wikipedia").Tag = 7;
+            treeView1.Nodes.Add("VİDEO");
+            treeView1.Nodes[2].Nodes.Add("Yotube").Tag=8;
+            treeView1.Nodes.Add("ALIŞVERİŞ");
+            treeView1.Nodes[3].Nodes.Add("Trendyol").Tag=9;
+            treeView1.Nodes[3].Nodes.Add("HepsiBurada").Tag=10;
+            treeView1.Nodes[3].Nodes.Add("Amazon").Tag=11;
+            treeView1.Nodes.Add("MÜZİK");
+            treeView1.Nodes[4].Nodes.Add("Spotiyf").Tag=12;
+        }
+
+        //private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        //{
+        //    //subnode link verme islemi
+        //    switch (e.Node.Text)
+        //    {
+        //        case "Instagram":
+        //            webView2.CoreWebView2.Navigate("https://www.instagram.com/");
+        //            break;
+        //        case "X":
+        //            webView2.CoreWebView2.Navigate("https://www.twitter.com/");
+        //            break;
+        //        case "Facebook":
+        //            webView2.CoreWebView2.Navigate("https://www.facebook.com/");
+        //            break;
+        //        case "Google":
+        //            webView2.CoreWebView2.Navigate("https://www.google.com.tr/");
+        //            break;
+        //        case "Yandex":
+        //            webView2.CoreWebView2.Navigate("https://www.yandex.com.tr/");
+        //            break;
+        //        case "Yahoo":
+        //            webView2.CoreWebView2.Navigate("https://www.yahoo.com.tr/");
+        //            break;
+        //        case "Yotube":
+        //            webView2.CoreWebView2.Navigate("https://www.youtube.com/");
+        //            break;
+        //        case "Amazon":
+        //            webView2.CoreWebView2.Navigate("https://www.amazon.com.tr/");
+        //            break;
+        //        case "Trendyol":
+        //            webView2.CoreWebView2.Navigate("https://www.trendyol.com.tr/");
+        //            break;
+        //        case "HepsiBurada":
+        //            webView2.CoreWebView2.Navigate("https://www.hepsiburada.com.tr/");
+        //            break;
+        //        case "Spotiyf":
+        //            webView2.CoreWebView2.Navigate("https://open.spotify.com/");
+        //            break;
+        //        default:
+
+        //            break;
+        //    }
+
+
+        //}
+       
+        private void Website(object Tag)
+        {
+            string url = "";
+
+            switch (Tag)
+            {
+                case 1:
+                    url = "https://www.instagram.com/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 2:
+                    url = "https://www.twitter.com/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 3:
+                    url = "https://www.facebook.com/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 4:
+                    url = "https://www.google.com.tr/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 5:
+                    url = "https://www.yandex.com.tr/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 6:
+                    url = "https://www.yahoo.com.tr/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 7:
+                    url = "https://www.wikipedia.org/";
+                    txt_ara.Visible = true;
+                    btn_ara.Visible = true;
+
+                    
+                        break;
+
+                    
+                case 8:
+                    url = "https://www.youtube.com/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false; 
+                case 9:
+                    url = "https://www.amazon.com.tr/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 10:
+                    url = "https://www.trendyol.com.tr/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                case 11:
+                    url = "https://www.hepsiburada.com.tr/";
+                    break;
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+                    
+
+                case 12:
+                    url = "https://open.spotify.com/";
+                    break;
+                default:
+                    txt_ara.Visible = false;
+                    btn_ara.Visible = false;
+
+                    return;
+                    
+            }
+            webView2.CoreWebView2.Navigate(url);
+        }
+
+      
+        private async void btn_ara_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+              
+                string searchText = txt_ara.Text;
+                string script1 = $"document.getElementById('searchInput').value = '{searchText}';";
+
+               
+                string script2 = "document.querySelector('.pure-button-primary-progressive').click();";
+
+                
+                await webView2.CoreWebView2.ExecuteScriptAsync(script1);
+
+               
+                await webView2.CoreWebView2.ExecuteScriptAsync(script2);
+            }
+        }
+     
+       
+        private async Task InitializeWebView2Async()
+        {
+            await webView2.EnsureCoreWebView2Async(null);
+            webView2.CoreWebView2.NavigationCompleted += webView2_NavigationCompleted;
+            webView2.MouseMove += webView2_MouseMove;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//git yerine url girmeek icin
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+                string searchText = textBox1.Text;
+                if (!searchText.StartsWith("http://") && !searchText.StartsWith("https://"))
+                {
+                    searchText = "https://www.google.com/search?q=" + Uri.EscapeDataString(searchText);
+                }
+                webView2.CoreWebView2.Navigate(searchText);
+                textBox1.Text = "";
+            }
+        }
+
+
+
+        private async void button2_Click(object sender, EventArgs e)
+        {//geri gitme ve buton rengini 1 saniye gri yapma
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+
+                webView2.CoreWebView2.GoBack();
+
+
+                button2.BackColor = Color.DarkGray;
+
+
+                await Task.Delay(1000);
+
+
+                button2.BackColor = SystemColors.Control;
+            }
+        }
+
+
+
+
+
+
+        private async void button3_Click(object sender, EventArgs e)
+        {//ileri islemi
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+                webView2.CoreWebView2.GoForward();
+
+                button3.BackColor = Color.DarkGray;
+
+                await Task.Delay(1000);
+
+                button3.BackColor = SystemColors.Control;
+            }
+        }
+
+
+        private async void button4_Click(object sender, EventArgs e)
+        {//yenileme islemi
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+                webView2.CoreWebView2.Reload();
+                button4.BackColor = Color.DarkGray;
+
+                await Task.Delay(1000); 
+
+                button4.BackColor = SystemColors.Control;
+            }
+        }
+
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+                webView2.CoreWebView2.Navigate("https://www.google.com.tr/");
+
+                await Task.Delay(1000);
+
+                button5.BackColor = SystemColors.Control;
+            }
+            txt_ara.Visible = false;
+            btn_ara.Visible = false;
+        }
+
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private async void webView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+                string script = @"
+                    document.querySelectorAll('a').forEach(function(link) {
+                        link.addEventListener('mouseover', function() {
+                            if (link.getAttribute('href') === '/Home/Athena/Corporate') {
+                                link.click();
+                            }s
+                        });
+                    });
+                ";
+                await webView2.CoreWebView2.ExecuteScriptAsync(script);
+            }
+        }
+
+        private async void webView2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (webView2 != null && webView2.CoreWebView2 != null)
+            {
+                string script = @"
+                    var aboutLink = document.querySelector('a[href=""/Home/Athena/Corporate""]');
+                    if (aboutLink) {
+                        aboutLink.click();
+                    }
+                ";
+                await webView2.CoreWebView2.ExecuteScriptAsync(script);
+            }
+        }
+
+       
+
+        private void treeView1_AfterSelect_1(object sender, TreeViewEventArgs e)
+        {
+            Website(e.Node.Tag);
+        }
+    }
+}
